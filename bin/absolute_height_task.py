a792b1d96c9d2f25a84fddbb1c2d5372ec7dec97
Jim Chiang <jchiang@slac.stanford.edu>
2015-11-27 07:55:23 -0800

    enable ITL analysis of absolute height using preset znom

--- bin/absolute_height_task.py
+++ bin/absolute_height_task.py
@@ -2,40 +2,49 @@
 
 import numpy as np
 import MetrologyData as metData
-from MetrologyData import md_factory
+from MetrologyData import md_factory, XyzPlane
 
-def absolute_height_task(sensor_id, infile, zoffset=0):
-    ogpData = md_factory.create(infile)
-    #
-    # Fit and set the reference plane to the gauge blocks.
-    #
-    ogpData.set_ref_plane(ogpData.refPlane_fit(), zoffset=zoffset)
+def absolute_height_task(sensor_id, infile, dtype='OGP', zoffset=0):
+    sensorData = md_factory.create(infile, dtype=dtype)
+    if dtype == 'OGP':
+        #
+        # Fit and set the reference plane to the gauge blocks.
+        #
+        sensorData.set_ref_plane(sensorData.refPlane_fit(), zoffset=zoffset)
+    elif dtype == 'ITL':
+        #
+        # Set reference plane at znom=12.998 mm
+        #
+        sensorData.set_ref_plane(XyzPlane(0, 0, 12998.))
+    else:
+        raise RuntimeError("%s not supported for absolute height analysis" 
+                           % dtype)
     #
     # Write surface height data points.
     #
     outfile = '%s_abs_height_residuals.txt' % sensor_id
-    ogpData.write_residuals(outfile)
+    sensorData.write_residuals(outfile)
     #
     # Make a histogram of residual heights.
     #
-    ogpData.plot_statistics(title='Sensor Absolute Height, %s' % infile)
+    sensorData.plot_statistics(title='Sensor Absolute Height, %s' % infile)
     metData.plot.save('%s_abs_height_hist.png' % sensor_id)
     #
     # Box and whisker plot of residual heights
     #
-    ogpData.resids_boxplot()
+    sensorData.resids_boxplot()
     metData.plot.save('%s_abs_height_boxplot.png' % sensor_id)
     #
     # Quantile table
     #
-    ogpData.quantile_table(outfile='%s_abs_height_quantile_table.txt'
+    sensorData.quantile_table(outfile='%s_abs_height_quantile_table.txt'
                            % sensor_id)
     #
     # Surface plots
     #
     azims = (10, 45)
     for azim in azims:
-        ogpData.absolute_height_plot(azim=azim)
+        sensorData.absolute_height_plot(azim=azim)
         metData.plot.save('%s_abs_height_point_cloud_azim_%i.png' 
                           % (sensor_id, azim))
 
