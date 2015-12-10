da8d226da8fbae0145b027799e69fd35b65948c7
Jim Chiang <jchiang@slac.stanford.edu>
2015-12-09 19:47:26 -0800

    use pylab_plotter.py module from eotest

--- python/MetrologyData.py
+++ python/MetrologyData.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import scipy.optimize
 from mpl_toolkits.mplot3d import Axes3D
-import pylab_plotter as plot
+import lsst.eotest.sensor.pylab_plotter as plot
 
 class XyzPlane(object):
     """
